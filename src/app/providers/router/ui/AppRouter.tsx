24f664af3e73049657b2bc616b88cbf6f5de4ef7
BearablePain <quasareat@gmail.com>
2023-02-14 03:54:18 +0300

    add not found page

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,20 +1,24 @@
 import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { routers } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'shared/ui/PageLoader/PageLoader';
 
 const AppRouter = () => (
-  <Suspense fallback={<div>Loading...</div>}>
-    <Routes>
-      {routers.map(({ element, path }) => (
+  <Routes>
+    {routers.map(({ element, path }) => (
 
-        <Route
-          key={path}
-          path={path}
-          element={<div className="page-wrapper">{element}</div>}
-        />
-      ))}
-    </Routes>
-  </Suspense>
+      <Route
+        key={path}
+        path={path}
+        element={(
+          <Suspense fallback={<PageLoader />}>
+            <div className="page-wrapper">{element}</div>
+            {' '}
+          </Suspense>
+        )}
+      />
+    ))}
+  </Routes>
 );
 
 export default AppRouter;
