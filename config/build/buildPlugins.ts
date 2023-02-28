d62902aac570802eea481537d3fa9cb6636c8dad
BearablePain <quasareat@gmail.com>
2023-03-01 02:31:02 +0300

    main GitHub pipeline action - fix 3

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,6 +3,7 @@ import webpack from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import ReactRefreshWebpackPluginfrom from '@pmmmwh/react-refresh-webpack-plugin';
 import { BuildOptions } from './types/config';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
 export function buildPlugins(
   { paths }: BuildOptions,
@@ -24,6 +25,9 @@ export function buildPlugins(
   if (isDev) {
     plugins.push(new webpack.HotModuleReplacementPlugin());
     plugins.push(new ReactRefreshWebpackPluginfrom());
+    plugins.push(new BundleAnalyzerPlugin({
+      openAnalyzer: false,
+    }));
   }
   return plugins;
 }
